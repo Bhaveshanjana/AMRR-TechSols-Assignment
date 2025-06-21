994a13d6d823cbc5b27d7aff110e21188f24e5d2
Bhaveshanjana <bhaveshanjana58@gmail.com>
2025-06-22 00:32:38 +0530

    update name

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -89,7 +89,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-4 text-center text-gray-400">
-          <p>&copy; 2025 StyleHub. All rights reserved.</p>
+          <p>&copy; 2025 Shopzy. All rights reserved.</p>
         </div>
       </div>
     </footer>
